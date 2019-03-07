fce1c578ce37b52ed45cb28e2249d8a0c9c21de5
Alex North <anorth@users.noreply.github.com>
2019-03-07 11:18:53 +1100

    Rename filnet directory to net and move pubsub and the plumbing network object under it.

--- net/network.go
+++ net/network.go
@@ -1,11 +1,10 @@
-package ntwk
+package net
 
 import (
 	"gx/ipfs/QmTu65MVbemtUxJEWgsTtzv9Zv9P8rvmqNA4eG9TrTRGYc/go-libp2p-peer"
 	"gx/ipfs/Qmd52WKRSwrBK5gUaJKawryZQ5by6UbNB8KVW2Zy6JtbyW/go-libp2p-host"
 
-	"github.com/filecoin-project/go-filecoin/filnet"
-	"github.com/filecoin-project/go-filecoin/pubsub"
+	"github.com/filecoin-project/go-filecoin/net/pubsub"
 )
 
 // Network is a unified interface for dealing with libp2p
@@ -13,11 +12,11 @@ type Network struct {
 	host host.Host
 	*pubsub.Subscriber
 	*pubsub.Publisher
-	*filnet.Router
+	*Router
 }
 
 // New returns a new Network
-func New(host host.Host, publisher *pubsub.Publisher, subscriber *pubsub.Subscriber, router *filnet.Router) *Network {
+func New(host host.Host, publisher *pubsub.Publisher, subscriber *pubsub.Subscriber, router *Router) *Network {
 	return &Network{
 		host:       host,
 		Subscriber: subscriber,
